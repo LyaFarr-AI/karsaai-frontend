e12cb8a913041902f8243a1ee4d272eab8c8c828
LyaFarr-AI <rafaelroberttungka@gmail.com>
2025-06-25 16:00:42 +0700

    Update app.py

--- frontend_karsaai/app.py
+++ frontend_karsaai/app.py
@@ -119,7 +119,7 @@ elif menu == "Pantun":
     if opsi_model == "Quality":
         st.caption("⚡ Quality (Direkomendasikan) – Output lebih bagus, tapi waktu generate 1–2 menit.")
     else:
-        st.caption("🚀 Fast (Mode Alpha – LSTM) – Output lebih cepat, tapi belum mendukung jenis pantun.")
+        st.caption("🚀 Fast (LSTM) – Output lebih cepat, tapi belum mendukung jenis pantun dan masih tahap pengembangan.")
 
     # Input pantun
     jenis = st.selectbox("Pilih jenis pantun", [
